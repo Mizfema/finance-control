cdbe8b7b7762e1d1b9af49e71dc19709d6eeac57
Mizael <mizaellmagumba@gmail.com>
2022-02-14 23:00:51 +0200

    table transation commit

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -3,7 +3,36 @@ import { Container } from "./style";
 export function TransactionTable() {
     return (
         <Container>
-            Transaction Table
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento web</td>
+                        <td className="deposit">12.500,00MZN</td>
+                        <td>Informatica</td>
+                        <td>14/02/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguer</td>
+                        <td className="withdraw">7.000,00MZN</td>
+                        <td>Casa</td>
+                        <td>14/02/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento web</td>
+                        <td className="deposit">12.500,00MZN</td>
+                        <td>Informatica</td>
+                        <td>14/02/2022</td>
+                    </tr>
+                </tbody>
+            </table>
         </Container>
     )
 }
--- src/components/TransactionTable/style.ts
+++ src/components/TransactionTable/style.ts
@@ -4,4 +4,36 @@ import styled from "styled-components";
 export const Container = styled.div`
     max-width: 1120px;
     margin: 0 auto;
+    margin-top: 3rem;
+
+    table {
+        width: 100%;
+        border-spacing: 0 0.5rem;
+    }
+
+    th {
+        text-align:left;
+        padding: 1rem 2rem;
+       color: var(--text-title) ;
+    }
+
+    td {
+        background-color: var(--shape);
+        color: var(--text-body);
+        padding: 1rem 2rem;
+        border-radius: 0.25rem;
+        
+
+        &:first-child {
+            color: #000;
+        }
+
+        &.deposit {
+            color: var(--green);
+        }
+
+        &.withdraw {
+            color: var(--red);
+        }
+    }
 `
